cfe3ca74ff598c13e0c0d460862c388e44748f5f
Minh Chau <37843448+minhchauk1k@users.noreply.github.com>
2019-08-19 12:14:04 +0700

    Update x.js

--- x.js
+++ x.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 var bot = new Discord.Client();
 
-x.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000);
 //const PORT = process.env.PORT || 3000;
 
 //liên kết file json
