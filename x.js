b539814e98e87776cc3491c429279be4ac243122
minhchauk1k <5751071002@st.utc2.edu.vn>
2019-08-19 20:23:04 +0700

    Update x.js

--- x.js
+++ x.js
@@ -33,7 +33,7 @@ const PREFIX = ".";
 bot.on('ready', function(){
   setInterval(() => {
     const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-    bot.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+    bot.user.setStatus(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
 }, 10000);
   console.log("Bot Yuki is now online!");	
 })
