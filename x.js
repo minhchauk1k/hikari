b3b494323ee5878302bf90a40cdbc24b18f9e314
minhchauk1k <5751071002@st.utc2.edu.vn>
2019-08-19 19:37:24 +0700

    fix từ đầu

--- x.js
+++ x.js
@@ -18,16 +18,8 @@ app.get('/', function(request, response) {
 //liên kết file json
 bot.msgs = require('./learning.json');
 
-const { Client, RichEmbed, Attachment} = require('discord.js');
-
 const PREFIX = ".";
 
-//list để random
-var fortunes = ["Có","Không","Có thể","Chắc chắn luôn!","Không thể nào!","Hên xui!","Khó nói lắm ~"];
-var sex = ["Nam","Nữ","Đực rựa!","Giống cái~","Giữa giữa","Lưỡng","Bí mật nha~"];
-var hug =['https://i.imgur.com/AhGtkI7.gif','https://i.imgur.com/iMVRt36.gif','https://i.imgur.com/DKrVJ4I.gif','https://i.imgur.com/P2k2IHY.gif','https://i.imgur.com/a3LEDQX.gif','https://i.imgur.com/dUnMS6C.gif','https://i.imgur.com/KHLkb3n.gif'];
-var kiss =['https://i.imgur.com/Rg9mwvf.gif','https://i.imgur.com/Jy11NQm.gif','https://i.imgur.com/ixKHvah.gif','https://i.imgur.com/qODq72W.gif','https://i.imgur.com/I9qS9N6.gif','https://i.imgur.com/ABwjzgD.gif','https://i.imgur.com/RoOcfNU.gif','https://i.imgur.com/jNWQvTO.gif'];
-var slap=['https://i.imgur.com/QFBjeCZ.gif','https://i.imgur.com/VsHdS0z.gif','https://i.imgur.com/14XwzmI.gif','https://i.imgur.com/zWt8FKS.gif','https://i.imgur.com/YDg2gNU.gif','https://i.imgur.com/W2RVSri.gif','https://i.imgur.com/yzHpR2H.gif','https://i.imgur.com/IUMXMvj.gif','https://i.imgur.com/Ib8DOPy.gif'];
 
 
 //Tải bot
@@ -54,75 +46,64 @@ bot.on("message", async message => {
   var yeucau = message.content.toLowerCase();
   if(bot.msgs[yeucau]!=null) message.channel.send(bot.msgs[yeucau].reply);
 
-    //tạo args ~ cắt bỏ tên bot và tách phần sau ra làm 2 ô chứa
-    // var args = message.content.substring(bot.user).split(" ");
-    switch (yeucau) {
-      case "yuki ơi":
-      case "yuki đâu rồi?":
-      case "yuki":
-        message.channel.send(`Dạ để em gọi Sếp Yuki <@484399853045022726>`);
-        break;
-      case "hướng dẫn":
-      case "help":
-      case "tutorial":
-        message.channel.send(`Ấn .help để xem hướng dẫn các câu lệnh bot Hikari ạ!`);
-        break;
-      case "nani":
-      case "nani!":
-      case "nà ní":  
-      try{
-         //Xóa đoạn gọi bot
-         message.delete(); //nếu delay nó xóa dòng dưới
-         var mention = message.mentions.users.first();
-         var embed = new RichEmbed();
-         //Mention 1 người trong Embed
-         embed.setDescription(`${message.author} NANI?!!!!!!!` || "None");
-         embed.setColor(0xFFC0CB);
-         embed.setImage('https://i.makeagif.com/media/8-25-2017/8UsD0s.gif');
-         message.channel.send(embed);
-      }catch(e){
-        console.log('Error Nani!:', e.stack);
-      }
-           
-        break;
-
-      case "chẹp chẹp":  
-      try{
-        
-         var mention = message.mentions.users.first();
-         var embed = new RichEmbed();
-         //Mention 1 người trong Embed
-         embed.setDescription(`${message.author} said "Chẹp chẹp"` || "None");
-         embed.setColor(0xFFC0CB);
-         embed.setImage('https://media1.tenor.com/images/cb45f9562719f5a789952cba8e2fe9b7/tenor.gif');
-           
-         message.channel.send(embed);
-//            //Xóa đoạn gọi bot
+//     //tạo args ~ cắt bỏ tên bot và tách phần sau ra làm 2 ô chứa
+//     // var args = message.content.substring(bot.user).split(" ");
+//     switch (yeucau) {
+//       case "nani":
+//       case "nani!":
+//       case "nà ní":  
+//       try{
+//          //Xóa đoạn gọi bot
 //          message.delete(); //nếu delay nó xóa dòng dưới
-         
-      }catch(e){
-        console.log('Error Chẹp!:', e.stack);
-      }
-        break;
+//          var mention = message.mentions.users.first();
+//          var embed = new RichEmbed();
+//          //Mention 1 người trong Embed
+//          embed.setDescription(`${message.author} NANI?!!!!!!!` || "None");
+//          embed.setColor(0xFFC0CB);
+//          embed.setImage('https://i.makeagif.com/media/8-25-2017/8UsD0s.gif');
+//          message.channel.send(embed);
+//       }catch(e){
+//         console.log('Error Nani!:', e.stack);
+//       }
+//         break;
 
-      case "f":  
-      try{
+//       case "chẹp chẹp":  
+//       try{
         
-         var mention = message.mentions.users.first();
-         var embed = new RichEmbed();
-         //Mention 1 người trong Embed
-         embed.setDescription(`${message.author} đã ấn "F" để thể hiện sự tiếc thương đến bạn!` || "None");
-         embed.setColor(0xFFC0CB);
-         embed.setImage('https://i.pinimg.com/originals/38/d2/14/38d214c232f7cc550d15bb915a3af406.gif');
-            //Xóa đoạn gọi bot
-         message.delete(); //nếu delay nó xóa dòng dưới
-         message.channel.send(embed);
+//          var mention = message.mentions.users.first();
+//          var embed = new RichEmbed();
+//          //Mention 1 người trong Embed
+//          embed.setDescription(`${message.author} said "Chẹp chẹp"` || "None");
+//          embed.setColor(0xFFC0CB);
+//          embed.setImage('https://media1.tenor.com/images/cb45f9562719f5a789952cba8e2fe9b7/tenor.gif');
+           
+//          message.channel.send(embed);
+// //            //Xóa đoạn gọi bot
+// //          message.delete(); //nếu delay nó xóa dòng dưới
          
-      }catch(e){
-        console.log('Error Chẹp!:', e.stack);
-      }
-        break;
-    }
+//       }catch(e){
+//         console.log('Error Chẹp!:', e.stack);
+//       }
+//         break;
+
+//       case "f":  
+//       try{
+        
+//          var mention = message.mentions.users.first();
+//          var embed = new RichEmbed();
+//          //Mention 1 người trong Embed
+//          embed.setDescription(`${message.author} đã ấn "F" để thể hiện sự tiếc thương đến bạn!` || "None");
+//          embed.setColor(0xFFC0CB);
+//          embed.setImage('https://i.pinimg.com/originals/38/d2/14/38d214c232f7cc550d15bb915a3af406.gif');
+//             //Xóa đoạn gọi bot
+//          message.delete(); //nếu delay nó xóa dòng dưới
+//          message.channel.send(embed);
+         
+//       }catch(e){
+//         console.log('Error Chẹp!:', e.stack);
+//       }
+//         break;
+//     }
 
     
   
@@ -136,92 +117,84 @@ bot.on("message", async message => {
   //trường hợp dấu nhận dạng + câu lệnh ví dụ .ping .hug
   switch (args[0].toLowerCase()) {
 
-    //giống hello world
-    case "ping":
-        message.channel.send("pong!");
-    break;
-
-    case "hug":
-    case "ôm":
-      try{
-     
-    
-      var mention = message.mentions.users.first();
-      var embed = new RichEmbed();
-      //Mention 1 người trong Embed
-      embed.setDescription(`${message.author} Muốn ôm bạn nè! ${mention}`);
-      embed.setColor(0xFFC0CB);
-      embed.setImage(hug[Math.floor(Math.random() * hug.length)]);
-               //Xóa đoạn gọi bot
-      message.delete(); //nếu delay nó xóa dòng dưới
-      message.channel.send(embed);
+    // case "hug":
+    // case "ôm":
+    //   try{
+    //    //Xóa đoạn gọi bot
+    //    message.delete(); //nếu delay nó xóa dòng dưới
+    //   var mention = message.mentions.users.first();
+    //   var embed = new RichEmbed();
+    //   //Mention 1 người trong Embed
+    //   embed.setDescription(`${message.author} Muốn ôm bạn nè! ${mention}`);
+    //   embed.setColor(0xFFC0CB);
+    //   embed.setImage(hug[Math.floor(Math.random() * hug.length)]);
+    //   message.channel.send(embed);
       
-      }catch(e){
-        console.log('Error Hug!:', e.stack);
-      }
+    //   }catch(e){
+    //     console.log('Error Hug!:', e.stack);
+    //   }
        
-    break;
+    // break;
 
-    case "slap":
-    case "tát":
-    case "hit":
-    case "đánh":
-      try{
-          var mention = message.mentions.users.first();
-          var embed = new RichEmbed();
-         //Mention 1 người trong Embed
-         embed.setDescription(`${message.author} Muốn tát ${mention} thật là đau! `);
-         embed.setColor(0xFFC0CB);
-         embed.setImage(slap[Math.floor(Math.random() * slap.length)]);
-//             //Xóa đoạn gọi bot
-//          message.delete(); //nếu delay nó xóa dòng dưới
-         message.channel.send(embed);
-      }catch(e){
-        console.log('Error Slap!:', e.stack);
-      }
-      break;
+//     case "slap":
+//     case "tát":
+//     case "hit":
+//     case "đánh":
+//       try{
+//           var mention = message.mentions.users.first();
+//           var embed = new RichEmbed();
+//          //Mention 1 người trong Embed
+//          embed.setDescription(`${message.author} Muốn tát ${mention} thật là đau! `);
+//          embed.setColor(0xFFC0CB);
+//          embed.setImage(slap[Math.floor(Math.random() * slap.length)]);
+// //             //Xóa đoạn gọi bot
+// //          message.delete(); //nếu delay nó xóa dòng dưới
+//          message.channel.send(embed);
+//       }catch(e){
+//         console.log('Error Slap!:', e.stack);
+//       }
+//       break;
 
-    case "kiss":
-    case "hôn":
-    case "hun":
-      try{
+    // case "kiss":
+    // case "hôn":
+    // case "hun":
+    //   try{
        
       
-        var mention = message.mentions.users.first();
-        var embed = new RichEmbed();
-        //Mention 1 người trong Embed
-        embed.setDescription(`${message.author} Đè bạn ra hun nè! ${mention}`);
-        embed.setColor(0xFFC0CB);
-        embed.setImage(kiss[Math.floor(Math.random() * kiss.length)]);
-           //Xóa đoạn gọi bot
-        message.delete(); //nếu delay nó xóa dòng dưới
-        message.channel.send(embed);
+    //     var mention = message.mentions.users.first();
+    //     var embed = new RichEmbed();
+    //     //Mention 1 người trong Embed
+    //     embed.setDescription(`${message.author} Đè bạn ra hun nè! ${mention}`);
+    //     embed.setColor(0xFFC0CB);
+    //     embed.setImage(kiss[Math.floor(Math.random() * kiss.length)]);
+    //        //Xóa đoạn gọi bot
+    //     message.delete(); //nếu delay nó xóa dòng dưới
+    //     message.channel.send(embed);
           
-      }catch(e){
-        console.log('Error Kiss!:', e.stack);
-      }
-      break;
+    //   }catch(e){
+    //     console.log('Error Kiss!:', e.stack);
+    //   }
+    //   break;
 
-    case "help":
-      //Xóa đoạn gọi bot
-      message.delete();
+    // case "help":
+    //   //Xóa đoạn gọi bot
+    //   message.delete();
     
-        //đọc file tutorial.txt
-        var fs = require('fs');
-        try{
-          fs.readFile('./tutorial.txt', 'utf-8', function (err, data) {
-            var embed = new RichEmbed();
-            embed.setDescription(data);
-            message.channel.send(embed);
-            console.log('Error Help!:', err.stack);
-          });
-        }catch(e){
-          console.log('Error Help!:', e.stack);
-        }
-      break;
+    //     //đọc file tutorial.txt
+    //     var fs = require('fs');
+    //     try{
+    //       fs.readFile('./tutorial.txt', 'utf-8', function (err, data) {
+    //         var embed = new RichEmbed();
+    //         embed.setDescription(data);
+    //         message.channel.send(embed);
+    //         console.log('Error Help!:', err.stack);
+    //       });
+    //     }catch(e){
+    //       console.log('Error Help!:', e.stack);
+    //     }
+    //   break;
     
     case "set":
-      //message.delete();
       var fs = require('fs');
       try{
         var argsedit = message.content.substring(".set ".length).split(" -> ");
@@ -239,48 +212,45 @@ bot.on("message", async message => {
       }
       break;
 
-    case "random":
-    case "rad":
-        if(args[1]&&args[2]){
-              var max = args[1];
-              var min = args[2];
-              if (max<args[2]){
-                max=args[2];
-                min= args[1];
-              }
-              min = Math.ceil(min);
-              max = Math.floor(max);
-              message.channel.send(Math.floor(Math.random() * (max - min + 1)) + min);
-            }else{
-              message.channel.send("I can't read your question!")
-          }
-        break;
+    // case "random":
+    // case "rad":
+    //     if(args[1]&&args[2]){
+    //           var max = args[1];
+    //           var min = args[2];
+    //           if (max<args[2]){
+    //             max=args[2];
+    //             min= args[1];
+    //           }
+    //           min = Math.ceil(min);
+    //           max = Math.floor(max);
+    //           message.channel.send(Math.floor(Math.random() * (max - min + 1)) + min);
+    //         }else{
+    //           message.channel.send("I can't read your question!")
+    //       }
+    //     break;
 
-    case "?":
-    if(args[1]){
-        message.channel.send(fortunes[Math.floor(Math.random() * fortunes.length)]);
-      }else{
-        message.channel.send("Không hiểu muốn hỏi gì!");
-    }
-    break;
+    // case "?":
+    // if(args[1]){
+    //     message.channel.send(fortunes[Math.floor(Math.random() * fortunes.length)]);
+    //   }else{
+    //     message.channel.send("Không hiểu muốn hỏi gì!");
+    // }
+    // break;
 
-    case "sex":
-    if(args[1]){
-        message.channel.send(sex[Math.floor(Math.random() * sex.length)]);
-      }else{
-        message.channel.send("Không hiểu muốn hỏi gì!");
-    }
-    break;
+    // case "sex":
+    // if(args[1]){
+    //     message.channel.send(sex[Math.floor(Math.random() * sex.length)]);
+    //   }else{
+    //     message.channel.send("Không hiểu muốn hỏi gì!");
+    // }
+    // break;
 
-    case "ping":
-        message.channel.send("pong!");
-    break;
 
     // //kèm tài liệu
     // const attachment = new Attachment('https://i.imgur.com/wOmoeF8.gif');
     // message.channel.send(attachment);
     // break;
-    default: message.channel.send("Xin ấn .help và xem lại câu lệnh!")
+    default: message.channel.send("Xin ấn .help và xem lại câu lệnh!");
   }
   
 })	
