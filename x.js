406b8dd8a0b9e3337271a5b7e6e9553f739c2f20
Minh Chau <37843448+minhchauk1k@users.noreply.github.com>
2019-08-19 12:11:31 +0700

    Update x.js

--- x.js
+++ x.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js");
 var bot = new Discord.Client();
 
-const PORT = process.env.PORT || 3000;
+x.listen(process.env.PORT || 3000);
+//const PORT = process.env.PORT || 3000;
 
 //liên kết file json
 bot.msgs = require('./learning.json');
